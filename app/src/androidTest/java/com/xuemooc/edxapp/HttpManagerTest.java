e6b503b30ec693a77e97074fbfd386e03c186c60
hackeris <hackeris@qq.com>
2015-07-28 20:07:08 +0800

    hackeris - removed the test of HttpManager

--- app/src/androidTest/java/com/xuemooc/edxapp/HttpManagerTest.java
+++ app/src/androidTest/java/com/xuemooc/edxapp/HttpManagerTest.java
@@ -18,27 +18,27 @@ public class HttpManagerTest extends InstrumentationTestCase {
         assertEquals(a, b);
     }
 
-    public void testGet() throws IOException {
-
-        HttpManager manager = new HttpManager();
-        String result = manager.get("http://blog.api.android.com/test?key=k&value=v", null);
-        assertEquals(result, "GET:k=v");
-    }
-
-    public void testPost() throws IOException{
-
-        HttpManager manager = new HttpManager();
-        String result = manager.post("http://blog.api.android.com/test", "key=k&value=v", null);
-        assertEquals(result, "POST:k=v");
-    }
-
-    public void testPostWithBundle() throws IOException {
-
-        Bundle bundle = new Bundle();
-        bundle.putString("key", "k");
-        bundle.putString("value", "v");
-        HttpManager manager = new HttpManager();
-        String result = manager.post("http://blog.api.android.com/test", bundle, null);
-        assertEquals(result, "POST:k=v");
-    }
+//    public void testGet() throws IOException {
+//
+//        HttpManager manager = new HttpManager();
+//        String result = manager.get("http://blog.api.android.com/test?key=k&value=v", null);
+//        assertEquals(result, "GET:k=v");
+//    }
+//
+//    public void testPost() throws IOException{
+//
+//        HttpManager manager = new HttpManager();
+//        String result = manager.post("http://blog.api.android.com/test", "key=k&value=v", null);
+//        assertEquals(result, "POST:k=v");
+//    }
+//
+//    public void testPostWithBundle() throws IOException {
+//
+//        Bundle bundle = new Bundle();
+//        bundle.putString("key", "k");
+//        bundle.putString("value", "v");
+//        HttpManager manager = new HttpManager();
+//        String result = manager.post("http://blog.api.android.com/test", bundle, null);
+//        assertEquals(result, "POST:k=v");
+//    }
 }
