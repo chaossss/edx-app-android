52afbb91637f887c93057a0c153abb685746c12a
Chaos <437700255@qq.com>
2015-09-24 17:21:58 +0800

    添加LRU内存缓存

---
+++ app/src/main/java/com/xuemooc/edxapp/image/cache/memory/LruMemoryCache.java
@@ -0,0 +1,110 @@
+package com.xuemooc.edxapp.image.cache.memory;
+
+import android.graphics.Bitmap;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * A cache that holds strong reference to a limited number of Bitmaps.Each time a Bitmap is accessed, it is moved to the head of a queue.
+ * When a Bitmap is added to a full cache, the Bitmap at the end of that queue is evicted
+ * Created by chaossss on 2015/9/24.
+ */
+public class LruMemoryCache implements MemoryCache{
+    private int size;
+    private final int maxCacheSize;
+    private final LinkedHashMap<String, Bitmap> lruCacheMap;
+
+    /**
+     * @param maxCacheSize Maximum sum of the sizes of the Bitmaps in this cache.
+     */
+    public LruMemoryCache(int maxCacheSize) {
+        if(maxCacheSize <= 0){
+            throw new IllegalArgumentException("max cache size couldn't less than 0");
+        }
+
+        this.maxCacheSize = maxCacheSize;
+        lruCacheMap = new LinkedHashMap<>(0, 0.75f, true);
+    }
+
+    @Override
+    public final boolean put(String key, Bitmap image) {
+        if(key == null || image == null){
+            throw new NullPointerException("key == null or image == null");
+        }
+
+        synchronized (this){
+            size += sizeOf(image);
+            Bitmap previous = lruCacheMap.put(key, image);
+
+            if(previous != null){
+                size -= sizeOf(previous);
+            }
+        }
+
+        trimToSize(maxCacheSize);
+        return true;
+    }
+
+    @Override
+    public final Bitmap get(String key) {
+        if(key == null){
+            throw new NullPointerException("key == null");
+        }
+
+        synchronized (this){
+            return lruCacheMap.get(key);
+        }
+    }
+
+    @Override
+    public final Bitmap remove(String key) {
+        if(key == null){
+            throw new NullPointerException("key == null");
+        }
+
+        synchronized (this){
+            Bitmap previous = lruCacheMap.remove(key);
+            if(previous != null){
+                size -= sizeOf(previous);
+            }
+            return previous;
+        }
+    }
+
+    @Override
+    public void clear() {
+        trimToSize(-1);
+    }
+
+    private int sizeOf(Bitmap image){
+        return image.getRowBytes() * image.getHeight();
+    }
+
+    private void trimToSize(int maxCacheSize){
+        while(true){
+            String key;
+            Bitmap image;
+
+            synchronized (this){
+                if(size < 0 || (lruCacheMap.isEmpty()) && size != 0){
+                    throw new IllegalStateException(getClass().getName() + "sizeOf() is reporting inconsistent results");
+                }
+
+                if(size <= maxCacheSize || lruCacheMap.isEmpty()){
+                    break;
+                }
+
+                Map.Entry<String, Bitmap> toEvict = lruCacheMap.entrySet().iterator().next();
+                if(toEvict == null){
+                    break;
+                }
+
+                key = toEvict.getKey();
+                image = toEvict.getValue();
+                lruCacheMap.remove(key);
+                size -= sizeOf(image);
+            }
+        }
+    }
+}
