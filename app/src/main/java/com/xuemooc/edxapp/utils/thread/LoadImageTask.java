d985ae65e0845ae47343088cef1497769421ecf4
Chaos <437700255@qq.com>
2015-10-03 16:02:04 +0800

    refactor

--- app/src/main/java/com/xuemooc/edxapp/utils/thread/LoadImageTask.java
+++ app/src/main/java/com/xuemooc/edxapp/utils/thread/LoadImageTask.java
@@ -8,8 +8,11 @@ import android.util.Log;
 
 import com.xuemooc.edxapp.http.interfaces.IWebMessage;
 import com.xuemooc.edxapp.utils.cache.disk.BaseDiskCache;
+import com.xuemooc.edxapp.utils.cache.disk.DiskCache;
 import com.xuemooc.edxapp.utils.cache.memory.LruMemoryCache;
+import com.xuemooc.edxapp.utils.cache.memory.MemoryCache;
 import com.xuemooc.edxapp.utils.download.BaseImageDownloader;
+import com.xuemooc.edxapp.utils.download.ImageDownloader;
 import com.xuemooc.edxapp.utils.handler.WebHandler;
 
 import java.io.IOException;
@@ -23,14 +26,19 @@ public class LoadImageTask implements Runnable {
     private Message msg;
     private WebHandler handler;
     private IWebMessage iWebMessage;
-    private BaseDiskCache diskCache;
-    private LruMemoryCache memoryCache;
-    private BaseImageDownloader imageDownloader;
+    private DiskCache diskCache;
+    private MemoryCache memoryCache;
+    private ImageDownloader imageDownloader;
 
     public LoadImageTask(Message msg, IWebMessage iWebMessage) {
         this.msg = msg;
         this.iWebMessage = iWebMessage;
-        handler = new WebHandler(this.iWebMessage);
+
+        initParams();
+    }
+
+    private void initParams(){
+        handler = new WebHandler(iWebMessage);
 
         diskCache = new BaseDiskCache();
         memoryCache = new LruMemoryCache();
@@ -40,28 +48,37 @@ public class LoadImageTask implements Runnable {
     @Override
     public void run() {
         String url = (String) msg.obj;
-        Bitmap bitmap = loadImageFromNative(url);
-
-        if(bitmap == null){
-            try {
-                Log.v("image-load", "load from web");
-                bitmap = imageDownloader.getStream(url);
-                diskCache.save(url, bitmap);
-                memoryCache.put(url, bitmap);
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        }
+        Bitmap bitmap = loadImage(url);
 
         Bundle b = new Bundle();
-        Message temp = new Message();
         b.putString("url", url);
+
+        Message temp = new Message();
         temp.setData(b);
         temp.obj = bitmap;
         temp.what = msg.what;
         handler.sendMessage(temp);
+    }
 
+    private Bitmap loadImage(String url){
+        Bitmap bitmap = loadImageFromNative(url);
 
+        return bitmap == null ? loadImageFromWeb(url) : bitmap;
+    }
+
+    private Bitmap loadImageFromWeb(String url){
+        Log.v("image-load", "load from web");
+        Bitmap bitmap = null;
+        try {
+            bitmap = imageDownloader.getStream(url);
+
+            diskCache.save(url, bitmap);
+            memoryCache.put(url, bitmap);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return bitmap;
     }
 
     private Bitmap loadImageFromNative(String url){
